0dfcf79a133649214d90a386724f5feb688761b7
Oliver Keyes <ironholds@gmail.com>
2014-04-10 09:51:03 -0700

    update awkstrings, add overwrite = TRUE to file_reader

--- Functions/file_reader.R
+++ Functions/file_reader.R
@@ -2,7 +2,7 @@
 file_reader <- function(file){
   
   #Move file into the current directory and unzip
-  file.copy(from = file, to = file.path(getwd(),"Data","dailydata.tsv.gz"))
+  file.copy(from = file, to = file.path(getwd(),"Data","dailydata.tsv.gz"), overwrite = TRUE)
   system("gunzip ./Data/dailydata.tsv.gz")
   
   #Awk the hell out of it
