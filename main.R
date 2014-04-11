0a5a3bbddb6d9c73b0a6d004a2fe0e279ff7d23e
Oliver Keyes <ironholds@gmail.com>
2014-04-11 09:19:23 -0700

    main.R amendments to log for 30 days

--- main.R
+++ main.R
@@ -1,34 +1,43 @@
+#Load in config files, functions
+to_dispose <- lapply(list.files(file.path(getwd(),"Functions"), full.names = TRUE, pattern = "*.R"),source)
+source("config.R")
+source("globalstrings.R")
+
 mobilemetrics <- function(){
   
-  #Load in config files, functions
-  to_dispose <- lapply(list.files(file.path(getwd(),"Functions"), full.names = TRUE),source)
-  source("config.R")
+  #RequestLog parser
+  rl_parse <- function(curfile, desired_columns = c("timestamp","x_forwarded","UA","device","os","browser","browser_version")){
+    
+    #Store the date
+    curdate <- substring(text = curfile,
+                         first = 48,
+                         last = 55)
+    
+    #Read in the latest file
+    dailydata <- file_reader(file = curfile)
+    
+    #Filter it, logging the filter results
+    dailydata <- loss_logger(x = dailydata)
+    
+    #Identify user agents
+    dailydata <- ua_parse(dailydata)
+    
+    #Return
+    return(dailydata[,desired_columns])
+    
+  }
   
   #List files
   filelist <- list.files(path = "/a/squid/archive/sampled",
                          pattern = ".gz$",
                          full.names = TRUE)
   
-  #Limit to the most recent one
-  curfile <- filelist[length(filelist)]
+  #Limit to the most recent month
+  curfiles <- filelist[(length(filelist)-29):length(filelist)]
   
-  #Store the date
-  curdate <- substring(text = curfile,
-                       first = 48,
-                       last = 55)
+  recent_data <- lapply(curfiles, rl_parse)
   
-  #Read in the latest file
-  dailydata <- file_reader(file = curfile)
+  recent_data <- do.call("rbind",recent_data)
   
-  #Filter it, logging the filter results
-  dailydata <- loss_logger(x = dailydata)
-  
-  #Run it through the tagger
-  taggeddata <- tagger(x = dailydata)
-  
-  #Transmogriphy the results
-  transmogriphy(x = taggeddata)
+  save(recent_data, file = "recent.RData")
 }
-
-Apps
--wikipanion
