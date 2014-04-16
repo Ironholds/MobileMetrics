fd4377a83d101065a5e3e0834c8cc727f3cdb63c
Oliver Keyes <ironholds@gmail.com>
2014-04-15 17:22:05 -0700

    bugfixes

--- Functions/custodiet.R
+++ Functions/custodiet.R
@@ -8,12 +8,15 @@ custodiet <- function(x, start, end, regex, name, ...){
   output.df$reg_results <- grepl(x = x, pattern = regex, ...)
   
   #Randomly sample 10,000 rows.
-  output.df <- output.df[sample(1:nrow(output.df, 10000)),]
+  output.df <- output.df[sample(1:nrow(output.df), 10000),]
+  
+  #Rename
+  names(output.df) <- c("substring","hit")
   
   #Write out
-  write.table(x = output.df, file = file.path(getwd(),"logs",curdate,paste(name,"log.tsv", sep = "_")),
+  write.table(x = output.df, file = file.path(getwd(),"Logs",curdate,paste(name,"log.tsv", sep = "_")),
               quote = TRUE, sep = "\t", row.names = FALSE)
   
   #Return the regex results
-  return(output.df$reg_results)
+  return(output.df$hit)
 }
