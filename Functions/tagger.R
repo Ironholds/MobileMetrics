8a86f5c965cbff23f1b0bd59525087994806145e
Oliver Keyes <ironholds@gmail.com>
2014-04-17 11:38:52 -0700

    add tagger

--- Functions/tagger.R
+++ Functions/tagger.R
@@ -1,10 +1,13 @@
-tagger <- function(x){
+#Tags and adds categories
+tagger <- function(x, regex, values,...){
   
+  #Run the regex over X, generating a resultant boolean vector
+  bools <- grepl(x = x, pattern = regex, ...)
   
+  #Replace bools
+  bools[TRUE] <- values[1]
+  bools[FALSE] <- values[2]
   
-  
-  
-  
-  
-  
+  #Return
+  return(bools)
 }
