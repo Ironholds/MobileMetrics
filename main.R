658266f15d7f181abc66555fec88187f1403487c
Oliver Keyes <ironholds@gmail.com>
2014-04-21 15:43:00 -0700

    add timing tests

--- main.R
+++ main.R
@@ -8,24 +8,44 @@ mobilemetrics <- function(){
   #RequestLog parser
   rl_parse <- function(curfile){
     
+    #Start timing test
+    time_test <- data.frame()
+    
     #Store the date
     curdate <<- substring(text = curfile, first = 47, last = 54)
     
     #Create the logging directory
     dir.create(file.path(getwd(),"Logs",curdate), showWarnings = FALSE)
     
+    
     #Read in the latest file
-    dailydata <- file_reader(file = curfile)
+    time_test <- rbind(time_test,system.time({
+      dailydata <- file_reader(file = curfile)
+    }))
     
     #Filter it, logging the filter results
-    dailydata <- loss_logger(x = dailydata)
+    time_test <- rbind(time_test, system.time({
+      dailydata <- loss_logger(x = dailydata)
+    }))
     
     #Identify user agents
-    dailydata <- ua_parse(dailydata)
+    time_test <- rbind(time_test, system.time({
+      dailydata <- ua_parse(dailydata)
+    }))
     
     #Handle initial tagging
-    dailydata <- tag_logger(x = dailydata)
+    time_test <- rbind(time_test, system.time({
+      dailydata <- tag_logger(x = dailydata)
+    }))
     
+    #Write out time_test
+    names(time_test) <- c("user.self","sys.self","elapsed","user.child","sys.child")
+    write.table(time_test,
+                file = file.path(getwd(),"Logs",curdate,"time_log.tsv"),
+                quote = TRUE,
+                sep = "\t",
+                row.names = FALSE)
+    #Return
     return(dailydata)
   }
   
