3bd0af83ccd5a9e92739947c7592bcacf413c578
Oliver Keyes <ironholds@gmail.com>
2014-04-23 16:42:23 -0700

    further amendment

--- Modules/web.R
+++ Modules/web.R
@@ -24,29 +24,12 @@ web <- function(x){
   
   names(app_v_web) <- c("timestamp","MobileSite","MobileApps","Android","iOS","unidentifiable","other")
   
-  #Split out and reshape
-  app_breakdown <- melt(app_v_web[,c("timestamp","MobileSite","MobileApps"),], id.vars = 1, measure.vars = 2:3)
-  os_breakdown <- melt(app_v_web[,c("timestamp","android","iOS","unidentifiable","other"),], id.vars = 1, measure.vars = 2:5)
+  #Split out, reshape and write
+  FileWrite(x = melt(app_v_web[,c("timestamp","MobileSite","MobileApps"),], id.vars = 1, measure.vars = 2:3),
+            filename = file.path(getwd(),"Output","mobile_by_provider.tsv"))
   
-  #Write
-  if(file.exists(file.path(getwd(),"Output","mobile_by_provider.tsv"))){
-    
-    write.table(x = app_breakdown, file = file.path(getwd(),"Output","mobile_by_provider.tsv"),
-                append = TRUE, quote = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE)
-  } else {
-    
-    write.table(x = app_breakdown, file = file.path(getwd(),"Output","mobile_by_provider.tsv"),
-                append = FALSE, quote = TRUE, sep = "\t", row.names = FALSE, col.names = TRUE)
-  }
-  if(file.exists(file.path(getwd(),"Output","apps_by_os.tsv"))){
-    
-    write.table(x = os_breakdown, file = file.path(getwd(),"Output","mobile_by_os.tsv"),
-                append = TRUE, quote = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE)
-  } else {
-    
-    write.table(x = os_breakdown, file = file.path(getwd(),"Output","mobile_by_os.tsv"),
-                append = FALSE, quote = TRUE, sep = "\t", row.names = FALSE, col.names = TRUE)
-  }
-  
+  FileWrite(x = melt(app_v_web[,c("timestamp","android","iOS","unidentifiable","other"),], id.vars = 1, measure.vars = 2:5),
+            filename = file.path(getwd(),"Output","mobile_by_provider.tsv"))
+
   return(invisible())
 }
